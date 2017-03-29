2783ce6afa4d343276898e835f0c4e122d150961
Eva Anker <E.S.Anker@student.tudelft.nl>
2017-03-29 15:43:01 +0200

    Removed return comments

--- src/main/java/me/previewcode/backend/api/v1/CommentsAPI.java
+++ src/main/java/me/previewcode/backend/api/v1/CommentsAPI.java
@@ -1,7 +1,5 @@
 package me.previewcode.backend.api.v1;
 
-import java.io.IOException;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -13,8 +11,6 @@ import org.kohsuke.github.GHIssueComment;
 import me.previewcode.backend.DTO.PRComment;
 import me.previewcode.backend.DTO.PRGroupComment;
 import me.previewcode.backend.DTO.PRLineComment;
-import me.previewcode.backend.DTO.PRresponseComment;
-import me.previewcode.backend.DTO.User;
 import me.previewcode.backend.services.FirebaseService;
 import me.previewcode.backend.services.GithubService;
 
@@ -44,15 +40,14 @@ public class CommentsAPI {
      *            The number of the pull request
      * @param comment
      *            The comment that is placed.
-     * @return The comment to be displayed in the frontend
      */
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Path("pr")
-    public PRresponseComment postPRComment(@PathParam("owner") String owner,
+    public void postPRComment(@PathParam("owner") String owner,
             @PathParam("name") String name, @PathParam("number") int number,
             PRComment comment) {
-        return setResponseComment(commentsService.postComment(owner, name, number, comment));
+        commentsService.postComment(owner, name, number, comment);
     }
 
     /**
@@ -66,22 +61,20 @@ public class CommentsAPI {
      *            The number of the pull request
      * @param comment
      *            The comment that is placed.
-     * @return The comment to be displayed in the frontend
      */
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Path("group")
-    public PRresponseComment postGroupComment(@PathParam("owner") String owner,
+    public void postGroupComment(@PathParam("owner") String owner,
             @PathParam("name") String name, @PathParam("number") int number,
             PRGroupComment comment) {
-       GHIssueComment newComment = commentsService.postComment(owner, name, number, comment);
-       firebaseService.setComments(owner, name, number, newComment.getId(),
-               comment.groupID);
-        return setResponseComment(newComment);
+        GHIssueComment newComment = commentsService.postComment(owner, name, number, comment);
+        firebaseService.setComments(owner, name, number, newComment.getId(),
+                comment.groupID);
     }
 
     /**
-     * Sets the the group comments
+     * Sets the the line comments
      *
      * @param owner
      *            The owner of the repository where the comment is placed
@@ -99,22 +92,5 @@ public class CommentsAPI {
                                               @PathParam("name") String name, @PathParam("number") int number,
                                               PRLineComment comment) {
         commentsService.postLineComment(owner, name, number, comment);
-    }
-    
-    private PRresponseComment setResponseComment(GHIssueComment comment) {
-        PRresponseComment responseComment = new PRresponseComment();
-        try {
-            responseComment.body = comment.getBody();
-            responseComment.created_at = comment.getCreatedAt();
-            User user = new User();
-            user.avatar_url = comment.getUser().getAvatarUrl();
-            user.html_url = comment.getUser().getHtmlUrl();
-            user.login = comment.getUser().getLogin();
-            responseComment.user = user;
-            return responseComment;
-
-        } catch (IOException e) {
-            throw new IllegalArgumentException(e);
-        }
     }
 }
